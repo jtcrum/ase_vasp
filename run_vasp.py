01222ad92b04ab3fc390dca08c6188a6a821a448
jtcrum <jtcrum@me.com>
2022-01-21 08:28:51 -0500

    fixed run_vasp.py

--- run_vasp.py
+++ run_vasp.py
@@ -5,13 +5,17 @@ import subprocess
 import sys
 from ase.io import read,write
 
+q = '*@@schneider_q16copt'
+pe = 'mpi-64'
+nprocs =' 64'
+
 cwd = os.getcwd()
 script = """#!/bin/bash
 module load vasp
 
 cd {cwd}
 
-vasp_neb
+mpirun -np {nprocs} vasp_neb
 #end """.format(**locals())
 
 qscript=os.path.join(cwd,'qscript')
@@ -19,10 +23,9 @@ f = open(qscript,'w')
 f.write(script)
 f.close()
 
-q = '*@@schneider_q16copt'
-pe = 'mpi-64 64'
 
-cmdlist = 'qsub -o {0} -j y -N {1} -q {2} -pe {3} qscript'.format(cwd,cwd.split('/')[-1],q,pe)
+
+cmdlist = 'qsub -o {0} -j y -N {1} -q {2} -pe {3} {4} qscript'.format(cwd,cwd.split('/')[-1],q,pe,nprocs)
 
 os.system(cmdlist)
 
